b3a8e4b731e687a544cbf96c9c072b4a16e71f8d
tylee <bpthess@naver.com>
2023-05-05 00:41:56 +0900

    모달창 닫는기능 구현

--- frontend/src/ImageInfo.js
+++ frontend/src/ImageInfo.js
@@ -16,14 +16,26 @@ class ImageInfo {
   setState(nextData) {
     this.data = nextData;
     this.render();
+    this.setClose(nextData.visible);
+  }
+
+  setClose(visible) {
+    if (!visible) this.$imageInfo.style.display = 'none'
+  }
+
+   closeImageInfo() {
+    this.setState({
+      visible: false,
+      image: undefined,
+    });
   }
 
   render() {
     if (this.data.visible) {
       const { name, url, temperament, origin } = this.data.image;
-
+      
       this.$imageInfo.innerHTML = `
-        <div class="content-wrapper">
+        <div id="modal" class="content-wrapper">
           <div class="title">
             <span>${name}</span>
             <div class="close">x</div>
@@ -38,5 +50,18 @@ class ImageInfo {
     } else {
       this.$imageInfo.style.display = "none";
     }
+
+    /**
+     * 모달창이 띄어진 상태에서 클릭 해야 하기 때문에 렌더함수 안에서 작성
+     */
+    this.$imageInfo.addEventListener('click', (e) => {
+      let targetClassNm = e.target.className
+      if (
+        targetClassNm.includes('ImageInfo') ||
+        targetClassNm === 'close'
+      ) {
+        this.closeImageInfo()
+      }
+    });
   }
 }
